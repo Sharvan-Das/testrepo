2941bf36d6d66913bb77c5e8aa090e773d08ba64
Sharvan-Das <70105173+Sharvan-Das@users.noreply.github.com>
2020-08-23 21:59:58 +0530

    Create testchild.py

---
+++ testchild.py
@@ -0,0 +1,5 @@
+## Editing child branch
+
+printf("Editing child branch")
+
+printf("used following link for creating child branch- "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0105EN-Coursera/labs/GitHub/GitHub2_Branch_Merge.md.html"")
