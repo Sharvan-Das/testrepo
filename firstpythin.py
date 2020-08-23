4b6d94e2f75b3ed7a10d260f3d80a0583ea8ba2d
Sharvan-Das <70105173+Sharvan-Das@users.noreply.github.com>
2020-08-23 21:45:56 +0530

    Create firstpythin.py

---
+++ firstpythin.py
@@ -0,0 +1,3 @@
+#Display the Output
+Printf("My first Python code in Git Hub")
+Printf("I followed following link of Coursra- https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0105EN-Coursera/labs/GitHub/GitHub1_Getting_Started.md.html to create this")
